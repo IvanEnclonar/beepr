8873565914302f4638adeb3863fbd98dde132038
IvanEnclonar <paulivanenclonar@gmail.com>
2023-02-26 11:19:18 +0800

    Third day commit. Added add order form and background icon for swipe function

--- pages/queue/index.tsx
+++ pages/queue/index.tsx
@@ -1,11 +1,16 @@
 import styles from '@/styles/Queue.module.css'
 import MenuIcon from '@mui/icons-material/Menu'
 import QrCodeIcon from '@mui/icons-material/QrCode'
+import { useState } from 'react';
 import Order from '../../components/order'
+import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import ClearIcon from '@mui/icons-material/Clear'
 
 
 export default function index() {
+    const [openForm, setOpenForm] = useState(false);
+
     return (
         <div>
             <nav className={styles.nav}>
@@ -19,8 +24,17 @@ export default function index() {
             </nav>
             <Order />
             <div className={styles.addContainer}>
-                <div className={styles.addButton}>
+                <div className={styles.addButton} onClick={() => { setOpenForm(!openForm) }}>
                     ADD AN ORDER
+                </div>
+            </div>
+            <div className={openForm ? styles.formContainer : styles.formClose}>
+                <div className={styles.formMask} onClick={() => { setOpenForm(!openForm) }}></div>
+                <div className={styles.formBody}>
+                    <ClearIcon sx={{ fontSize: 35 }} />
+                    <TextField fullWidth id="outlined-basic" label="Order Name" variant="outlined" />
+                    <TextField fullWidth id="outlined-basic" label="Description" variant="outlined" />
+                    <Button variant="contained" className={styles.formButton}>Create</Button>
                 </div>
             </div>
         </div>
